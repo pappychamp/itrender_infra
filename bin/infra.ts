88a671ee562073fe72d5d505364d9f0924655d64
pappychamp <ka3.no3pappy@gmail.com>
2024-10-20 20:35:05 +0900

    third commit

--- bin/infra.ts
+++ bin/infra.ts
@@ -18,4 +18,7 @@ new NetworkStack(app, 'Itrender', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  synthesizer: new cdk.DefaultStackSynthesizer({
+    generateBootstrapVersionRule: false,
+}),
 });
--- lib/network-stack.ts
+++ lib/network-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class NetworkStack extends cdk.Stack {
@@ -8,6 +7,7 @@ export class NetworkStack extends cdk.Stack {
     super(scope, id, props);
     const vpc = new ec2.Vpc(this, 'VPC', {
         maxAzs: 1, // 使用するアベイラビリティゾーンの数
+        restrictDefaultSecurityGroup:false,
         subnetConfiguration: [
           {
             cidrMask: 24,
