61b8c242fd8fa7d4dc72796c17576ab20afca1d9
pappychamp <ka3.no3pappy@gmail.com>
2024-10-21 16:13:36 +0900

    add: vscodeによる自動整形

--- bin/infra.ts
+++ bin/infra.ts
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { NetworkResources } from '../lib/network';
-import { ECRResources } from '../lib/ecr';
-import { SecurityGroupResources } from '../lib/sg';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { NetworkResources } from "../lib/network";
+import { ECRResources } from "../lib/ecr";
+import { SecurityGroupResources } from "../lib/sg";
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'Itrender' ,{
+const stack = new cdk.Stack(app, "Itrender", {
   synthesizer: new cdk.DefaultStackSynthesizer({
     generateBootstrapVersionRule: false,
-}),
+  }),
 });
 
-
 // ネットワークリソースを追加
-const networkResources =new NetworkResources(stack, 'NetworkResources');
+const networkResources = new NetworkResources(stack, "NetworkResources");
 
 // ECRリソースを追加
-new ECRResources(stack, 'ECRResources');
-new SecurityGroupResources(stack, 'SecurityGroupResources',networkResources.vpc);
+new ECRResources(stack, "ECRResources");
+new SecurityGroupResources(
+  stack,
+  "SecurityGroupResources",
+  networkResources.vpc
+);
--- lib/sg.ts
+++ lib/sg.ts
@@ -1,16 +1,20 @@
-import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class SecurityGroupResources extends Construct {
   constructor(scope: Construct, id: string, vpc: ec2.Vpc) {
     super(scope, id);
-    
-    // ECRリポジトリの作成
-    const TestSG = new ec2.SecurityGroup(this, 'TestSG', {
-        vpc,
-        description: "test sg",
-      });
 
-      TestSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic');
+    // ECRリポジトリの作成
+    const TestSG = new ec2.SecurityGroup(this, "TestSG", {
+      vpc,
+      description: "test sg",
+    });
+
+    TestSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      "Allow HTTP traffic"
+    );
   }
 }
