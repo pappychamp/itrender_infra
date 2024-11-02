93330f763b30d4fbb1dc14e799817b00d8775ee8
pappychamp <ka3.no3pappy@gmail.com>
2024-11-02 18:29:27 +0900

    add: cloudfront

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,8 +1,9 @@
 import { Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
 import { siteDomain } from "./constants";
-
 export class S3Resources extends Construct {
+  public readonly samplewebsiteBucket: Bucket;
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
@@ -20,6 +21,8 @@ export class S3Resources extends Construct {
       },
       encryption: BucketEncryption.S3_MANAGED, // S3の暗号化を有効化
     });
+
+    this.samplewebsiteBucket = new Bucket(this, "samplewebsiteBucket");
 
     const samBucket = new Bucket(this, "SAMBucket", {
       bucketName: "itrender-sam-bucket", // バケット名を指定
