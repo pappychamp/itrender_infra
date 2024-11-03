fe2021be1c756230fa0b27d0ec26072af7a346c3
pappychamp <ka3.no3pappy@gmail.com>
2024-11-03 19:03:57 +0900

    add: apigateway,lambda

---
+++ lib/apigateway.ts
@@ -0,0 +1,25 @@
+import * as apigateway from "aws-cdk-lib/aws-apigatewayv2";
+import { Construct } from "constructs";
+import { LambdaResources } from "./lambda";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+
+export class ApiGatewayResources extends Construct {
+  constructor(scope: Construct, id: string, lambdaResources: LambdaResources) {
+    super(scope, id);
+
+    // API Gateway を作成
+    const backendApiGW = new apigateway.HttpApi(this, "BackendApiGW", {
+      apiName: "BackendApiGW",
+    });
+
+    // API Gateway ルートを定義
+    backendApiGW.addRoutes({
+      path: "/{proxy+}",
+      methods: [apigateway.HttpMethod.ANY],
+      integration: new HttpLambdaIntegration(
+        "BackendIntegration",
+        lambdaResources.lambdaFunction
+      ),
+    });
+  }
+}
