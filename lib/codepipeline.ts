aa1706e1d952c6a01673b6647aee0c6914c50d56
pappychamp <ka3.no3pappy@gmail.com>
2024-11-14 20:39:07 +0900

    add: batch ecs codepipeline

--- lib/codepipeline.ts
+++ lib/codepipeline.ts
@@ -5,7 +5,8 @@ import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
 import { ECRResources } from "./ecr";
 import { LambdaResources } from "./lambda";
 import * as iam from "aws-cdk-lib/aws-iam";
-
+import { SNSResources } from "./sns";
+import { ECSResources } from "./ecs";
 export class CodePipelineResources extends Construct {
   public readonly backendPipeline: codepipeline.Pipeline;
   constructor(
@@ -13,12 +14,14 @@ export class CodePipelineResources extends Construct {
     id: string,
     ecrResources: ECRResources,
     lambdaResources: LambdaResources,
-    region: string,
-    accountId: string
+    ecsResources: ECSResources
   ) {
     super(scope, id);
 
     const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+
+    // =====Backendのパイプライン=====
 
     // backendPipelineで使用するbuildプロジェクト
     const backendLambdaUpdateProject = new codebuild.PipelineProject(
@@ -79,6 +82,83 @@ export class CodePipelineResources extends Construct {
               actionName: "Lambda-Update",
               project: backendLambdaUpdateProject,
               input: sourceOutput,
+            }),
+          ],
+        },
+      ],
+    });
+
+    // =====Batchのパイプライン=====
+    // backendPipelineで使用するbuildプロジェクト
+    const batchECSUpdateProject = new codebuild.PipelineProject(
+      this,
+      `BatchECSUpdateProject`,
+      {
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: "0.2",
+          phases: {
+            build: {
+              commands: [
+                // imageのハッシュを取得
+                `imageDigest=$(aws ecr describe-images --repository-name ${ecrResources.batchRepository.repositoryName} | jq -r '.imageDetails[] | select(.imageTags == ["latest"]) | .imageDigest')`,
+                `echo "Image Digest: $imageDigest"`,
+                // imagedefinitions.json作成
+                `echo '[{"name": "BatchTask", "imageUri": "${ecrResources.batchRepository.repositoryUri}@$imageDigest"}]' > imagedefinitions.json`,
+                `echo "imagedefinitions.json 内容:"`,
+                `cat imagedefinitions.json`,
+              ],
+            },
+          },
+          artifacts: {
+            files: [
+              "imagedefinitions.json", // imagedefinitions.jsonを出力
+            ],
+          },
+        }),
+        environment: {
+          buildImage:
+            codebuild.LinuxLambdaBuildImage.AMAZON_LINUX_2023_CORRETTO_21,
+        },
+      }
+    );
+
+    // CodeBuildプロジェクトにECRのイメージ詳細取得とpull権限とLambda更新権限を付与
+    ecrResources.batchRepository.grantPull(batchECSUpdateProject);
+    ecrResources.batchRepository.grantRead(batchECSUpdateProject);
+
+    this.backendPipeline = new codepipeline.Pipeline(this, "BatchPipeline", {
+      pipelineName: "BatchPipeline",
+      crossAccountKeys: false,
+      stages: [
+        {
+          stageName: "Source",
+          actions: [
+            new codepipeline_actions.EcrSourceAction({
+              actionName: "ECR-Pull",
+              repository: ecrResources.batchRepository,
+              imageTag: "latest",
+              output: sourceOutput,
+            }),
+          ],
+        },
+        {
+          stageName: "Build",
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: "imagedefinitions-Create",
+              project: batchECSUpdateProject,
+              input: sourceOutput,
+              outputs: [buildOutput],
+            }),
+          ],
+        },
+        {
+          stageName: "Deploy",
+          actions: [
+            new codepipeline_actions.EcsDeployAction({
+              actionName: "ECS-Update",
+              service: ecsResources.ecsService,
+              input: buildOutput,
             }),
           ],
         },
